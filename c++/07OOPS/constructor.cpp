3cf37bf226e5ede099cd8086ee5ba3740bdfb34d
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-07-20 01:52:24 +0530

    constructor

---
+++ c++/07OOPS/constructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class bike
+{
+private:
+public:
+    int tyreSize;
+    // default constructor
+    bike()
+    {
+        cout<<"constructor call hua he ";
+    }
+};
+
+int main()
+{
+    bike tvs;
+    return 0;
+}
